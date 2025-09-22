114b776745fa029e0c8b3a364aa5cdf1c2ea42bb
eminamumic <emimumic1@gmail.com>
2025-09-22 19:48:01 +0200

    Add Home route to app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,21 @@ import React from 'react'
 import './i18n'
 import { Container, Box } from '@mui/material'
 import Header from './components/Header'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
 
 const App: React.FC = () => {
   return (
-    <>
+    <Router>
       <Header />
       <Container maxWidth="lg" sx={{ mt: 4 }}>
-        <Box></Box>
+        <Box>
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </Box>
       </Container>
-    </>
+    </Router>
   )
 }
 
