feefe78a395b814b2cc0f678e414a1eb0fd5b136
eminamumic <emimumic1@gmail.com>
2025-09-22 20:14:37 +0200

    feat: add modal dialog for creating and editing users

--- src/pages/Administration.tsx
+++ src/pages/Administration.tsx
@@ -10,6 +10,12 @@ import {
   TableHead,
   TableRow,
   Paper,
+  Button,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+  TextField,
 } from '@mui/material'
 
 interface User {
@@ -28,11 +34,44 @@ const Administration: React.FC = () => {
   const { t } = useTranslation()
   const [users, setUsers] = useState<User[]>(mockUsers)
 
+  const [openDialog, setOpenDialog] = useState(false)
+  const [currentUser, setCurrentUser] = useState<User | null>(null)
+  const [userName, setUserName] = useState('')
+  const [userLastName, setUserLastName] = useState('')
+
+  const handleCreateNewUser = () => {
+    setCurrentUser(null)
+    setUserName('')
+    setUserLastName('')
+    setOpenDialog(true)
+  }
+
+  const handleEditUser = (user: User) => {
+    setCurrentUser(user)
+    setUserName(user.name)
+    setUserLastName(user.lastName)
+    setOpenDialog(true)
+  }
+
+  const handleCloseDialog = () => {
+    setOpenDialog(false)
+  }
+
   return (
     <Box sx={{ p: 3 }}>
       <Typography variant="h4" component="h1" gutterBottom>
         {t('administration_view')}
       </Typography>
+
+      <Box sx={{ display: 'flex', justifyContent: 'flex-end', mb: 2 }}>
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={handleCreateNewUser}
+        >
+          {t('create_new_user')}
+        </Button>
+      </Box>
 
       <TableContainer component={Paper}>
         <Table>
@@ -54,6 +93,37 @@ const Administration: React.FC = () => {
           </TableBody>
         </Table>
       </TableContainer>
+
+      <Dialog open={openDialog} onClose={handleCloseDialog}>
+        <DialogTitle>
+          {currentUser ? t('edit_user') : t('create_user_title')}
+        </DialogTitle>
+        <DialogContent>
+          <TextField
+            autoFocus
+            margin="dense"
+            label={t('name')}
+            type="text"
+            fullWidth
+            variant="standard"
+            value={userName}
+            onChange={(e) => setUserName(e.target.value)}
+          />
+          <TextField
+            margin="dense"
+            label={t('last_name')}
+            type="text"
+            fullWidth
+            variant="standard"
+            value={userLastName}
+            onChange={(e) => setUserLastName(e.target.value)}
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleCloseDialog}>{t('cancel')}</Button>
+          <Button onClick={handleCloseDialog}>{t('save')}</Button>
+        </DialogActions>
+      </Dialog>
     </Box>
   )
 }
