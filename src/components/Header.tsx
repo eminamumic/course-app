3a305c69d79d1214e4405ed0e534dee4941558c9
eminamumic <emimumic1@gmail.com>
2025-09-22 19:40:22 +0200

    Add languageSwitcher to Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,23 +1,25 @@
 import React from 'react'
-
+import { useTranslation } from 'react-i18next'
 import AppBar from '@mui/material/AppBar'
 import Toolbar from '@mui/material/Toolbar'
 import Typography from '@mui/material/Typography'
 import IconButton from '@mui/material/IconButton'
 import MenuIcon from '@mui/icons-material/Menu'
+import LanguageSwitcher from './LanguageSwitcher'
 
 const Header: React.FC = () => {
+  const { t } = useTranslation()
+
   return (
     <AppBar position="static" sx={{ backgroundColor: 'black' }}>
       <Toolbar>
         <IconButton edge="start" color="inherit" aria-label="menu">
           <MenuIcon />
         </IconButton>
-        <Typography
-          variant="h6"
-          component="div"
-          sx={{ flexGrow: 1 }}
-        ></Typography>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          {t('app_name')}
+        </Typography>
+        <LanguageSwitcher />
       </Toolbar>
     </AppBar>
   )
