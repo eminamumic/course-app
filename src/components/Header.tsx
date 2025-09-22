c04298b242f89c757ecb92640413d91533af1070
eminamumic <emimumic1@gmail.com>
2025-09-22 21:23:06 +0200

    Add hamburger menu to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,17 +4,18 @@ import AppBar from '@mui/material/AppBar'
 import Toolbar from '@mui/material/Toolbar'
 import Typography from '@mui/material/Typography'
 import IconButton from '@mui/material/IconButton'
-import MenuIcon from '@mui/icons-material/Menu'
+
 import LanguageSwitcher from './LanguageSwitcher'
+import HamburgerMenu from './Hamburger'
 
 const Header: React.FC = () => {
   const { t } = useTranslation()
 
   return (
-    <AppBar position="static" sx={{ backgroundColor: 'black' }}>
+    <AppBar position="static">
       <Toolbar>
         <IconButton edge="start" color="inherit" aria-label="menu">
-          <MenuIcon />
+          <HamburgerMenu></HamburgerMenu>
         </IconButton>
         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
           {t('app_name')}
