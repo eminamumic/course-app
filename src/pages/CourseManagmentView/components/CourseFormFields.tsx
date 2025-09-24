f12baae8b57df3500a391b28589309c43078a9da
eminamumic <emimumic1@gmail.com>
2025-09-24 20:45:24 +0200

    Add internationalization (i18n) to CourseFormFields component

--- src/pages/CourseManagmentView/components/CourseFormFields.tsx
+++ src/pages/CourseManagmentView/components/CourseFormFields.tsx
@@ -12,6 +12,7 @@ import {
   MenuItem,
 } from '@mui/material'
 import type { CourseFormValues } from '../CourseManagment.types'
+import { useTranslation } from 'react-i18next'
 
 interface CourseFormFieldsProps {
   formValues: CourseFormValues
@@ -30,12 +31,21 @@ export const CourseFormFields = ({
   onCheckboxChange,
   errors,
 }: CourseFormFieldsProps) => {
+  const { t } = useTranslation()
+
   return (
     <>
-      <Card sx={{ mb: 4, boxShadow: 'none', border: '1px solid #e0e0e0' }}>
-        <CardContent>
-          <Typography variant="h6" gutterBottom>
-            Basic data
+      <Card
+        sx={{
+          mb: 4,
+          boxShadow: 'none',
+          border: '1px solid #e0e0e0',
+          fontFamily: 'serif',
+        }}
+      >
+        <CardContent sx={{ fontFamily: 'serif' }}>
+          <Typography variant="h6" gutterBottom sx={{ fontFamily: 'serif' }}>
+            {t('basic_data')}
           </Typography>
           <Box
             sx={{
@@ -48,22 +58,24 @@ export const CourseFormFields = ({
             <Box sx={{ flex: 1, minWidth: { xs: 'auto', sm: 250 } }}>
               <TextField
                 fullWidth
-                label="Course name"
+                label={t('course_name')}
                 name="courseName"
                 value={formValues.courseName}
                 onChange={onInputChange}
                 error={!!errors.courseName}
                 helperText={errors.courseName}
+                sx={{ fontFamily: 'serif' }}
               />
             </Box>
             <Box sx={{ flex: 1, minWidth: { xs: 'auto', sm: 250 } }}>
               <TextField
                 fullWidth
-                label="Number of participants"
+                label={t('number_of_participants')}
                 name="numberOfParticipants"
                 type="number"
                 value={formValues.numberOfParticipants}
                 onChange={onInputChange}
+                sx={{ fontFamily: 'serif' }}
               />
             </Box>
           </Box>
@@ -72,29 +84,33 @@ export const CourseFormFields = ({
 
       <Card sx={{ mb: 4, boxShadow: 'none', border: '1px solid #e0e0e0' }}>
         <CardContent>
-          <Typography variant="h6" gutterBottom>
-            Course classification
+          <Typography variant="h6" gutterBottom sx={{ fontFamily: 'serif' }}>
+            {t('course_classification')}
           </Typography>
           <RadioGroup
             row
             name="courseClassification"
             value={formValues.courseClassification}
             onChange={onRadioChange}
+            sx={{ fontFamily: 'serif' }}
           >
             <FormControlLabel
               value="Technical"
               control={<Radio />}
-              label="Technical"
+              label={t('technical')}
+              sx={{ fontFamily: 'serif' }}
             />
             <FormControlLabel
               value="Soft skills"
               control={<Radio />}
-              label="Soft skills"
+              label={t('soft_skills')}
+              sx={{ fontFamily: 'serif' }}
             />
             <FormControlLabel
               value="Business"
               control={<Radio />}
-              label="Business"
+              label={t('business')}
+              sx={{ fontFamily: 'serif' }}
             />
           </RadioGroup>
         </CardContent>
@@ -102,8 +118,8 @@ export const CourseFormFields = ({
 
       <Card sx={{ mb: 4, boxShadow: 'none', border: '1px solid #e0e0e0' }}>
         <CardContent>
-          <Typography variant="h6" gutterBottom>
-            Course department
+          <Typography variant="h6" gutterBottom sx={{ fontFamily: 'serif' }}>
+            {t('course_department')}
           </Typography>
           <TextField
             select
@@ -111,19 +127,28 @@ export const CourseFormFields = ({
             name="courseDepartment"
             value={formValues.courseDepartment}
             onChange={onInputChange}
+            sx={{ fontFamily: 'serif' }}
           >
-            <MenuItem value="All">All</MenuItem>
-            <MenuItem value="Java">Java</MenuItem>
-            <MenuItem value=".NET">.NET</MenuItem>
-            <MenuItem value="SAP">SAP</MenuItem>
+            <MenuItem value="All" sx={{ fontFamily: 'serif' }}>
+              {t('all')}
+            </MenuItem>
+            <MenuItem value="Java" sx={{ fontFamily: 'serif' }}>
+              {t('java')}
+            </MenuItem>
+            <MenuItem value=".NET" sx={{ fontFamily: 'serif' }}>
+              {t('.net')}
+            </MenuItem>
+            <MenuItem value="SAP" sx={{ fontFamily: 'serif' }}>
+              {t('sap')}
+            </MenuItem>
           </TextField>
         </CardContent>
       </Card>
 
       <Card sx={{ mb: 4, boxShadow: 'none', border: '1px solid #e0e0e0' }}>
         <CardContent>
-          <Typography variant="h6" gutterBottom>
-            Course participant group
+          <Typography variant="h6" gutterBottom sx={{ fontFamily: 'serif' }}>
+            {t('course_participant_group')}
           </Typography>
           <FormGroup row>
             <FormControlLabel
@@ -132,11 +157,13 @@ export const CourseFormFields = ({
                   checked={formValues.participantGroups.developers}
                   onChange={onCheckboxChange}
                   name="developers"
+                  sx={{ fontFamily: 'serif' }}
                 />
               }
-              label="Developers"
+              label={t('developers')}
             />
             <FormControlLabel
+              sx={{ fontFamily: 'serif' }}
               control={
                 <Checkbox
                   checked={formValues.participantGroups.managers}
@@ -144,9 +171,10 @@ export const CourseFormFields = ({
                   name="managers"
                 />
               }
-              label="Managers"
+              label={t('managers')}
             />
             <FormControlLabel
+              sx={{ fontFamily: 'serif' }}
               control={
                 <Checkbox
                   checked={formValues.participantGroups.hr}
@@ -154,9 +182,10 @@ export const CourseFormFields = ({
                   name="hr"
                 />
               }
-              label="HR"
+              label={t('hr')}
             />
             <FormControlLabel
+              sx={{ fontFamily: 'serif' }}
               control={
                 <Checkbox
                   checked={formValues.participantGroups.administration}
@@ -164,7 +193,7 @@ export const CourseFormFields = ({
                   name="administration"
                 />
               }
-              label="Administration"
+              label={t('administration')}
             />
           </FormGroup>
         </CardContent>
